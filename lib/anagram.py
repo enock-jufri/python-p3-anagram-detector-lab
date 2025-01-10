90f1110ee1d4283d2191d3de4fdc4a9db8e4393b
enock-jufri <enockjufri@gmail.com>
2025-01-10 12:04:52 +0300

    finds anagram in list using class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+    
+    def match(self,list):
+        self.list=list
+        return [i for i in list if sorted(i)==sorted(self.word)]
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
+
+
